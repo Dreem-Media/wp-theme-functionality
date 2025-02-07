5567c92fb33148b7f14b0d378ae88a09002cc3f4
Alex Whitlock <zanderwhitlock@gmail.com>
2025-02-07 22:41:19 +0000

    Testing out global functions

---
+++ functions.php
@@ -0,0 +1,4 @@
+<?php
+// This file will be auto-loaded by Composer (via the "files" autoload directive).
+
+require_once __DIR__ . '/src/Functions/helpers.php';
---
+++ src/Functions/helpers.php
@@ -0,0 +1,14 @@
+<?php
+
+if (! function_exists('patter_post_title_2')) {
+    function patter_post_title_2($post_id = null)
+    {
+        // Example logic: You can call other helper functions or work with ACF, etc.
+        $title = get_post_meta($post_id ?? get_the_ID(), 'patter_field_post__page_headline_h1', true);
+        if (! $title) {
+            // Fallback to the post title.
+            $title = get_the_title($post_id);
+        }
+        echo $title;
+    }
+}
